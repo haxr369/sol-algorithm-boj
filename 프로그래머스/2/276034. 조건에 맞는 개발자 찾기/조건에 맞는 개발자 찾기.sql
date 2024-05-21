3edabb9d9918e661d53a2dfe77d1d5bb83ca0941
오찬솔 <53365713+haxr369@users.noreply.github.com>
2024-05-21 11:41:10 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+SELECT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME FROM DEVELOPERS d
+WHERE d.SKILL_CODE&(
+        SELECT CODE FROM SKILLCODES s
+        WHERE s.NAME = "Python"
+    ) > 0 OR
+    d.SKILL_CODE&(
+        SELECT CODE FROM SKILLCODES s
+        WHERE s.NAME = "C#"
+    ) > 0
+ORDER BY ID ASC;
