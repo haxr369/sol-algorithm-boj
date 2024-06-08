8638fa88f4364c4ec3c40c8359feb70fe9e333cc
오찬솔 <53365713+haxr369@users.noreply.github.com>
2024-06-08 14:16:09 +0900

    Update 특정 세대의 대장균 찾기.sql

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -8,4 +8,21 @@ INNER JOIN ECOLI_DATA ed2
 INNER JOIN ECOLI_DATA ed1
     ON ed2.PARENT_ID = ed1.ID AND ed1.PARENT_ID IS NULL
 ORDER BY ed3.ID ASC;
+
+--- 재귀적인 방법으로 계층구조를 만들 수 있다.
+
+WITH RECURSIVE HierarchicalData AS (
+    SELECT ID, PARENT_ID, 1 AS HIGH
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
     
+    UNION ALL
+    
+    SELECT ed.ID, ed.PARENT_ID, (hd.HIGH+1) AS HIGH
+    FROM ECOLI_DATA ed
+    INNER JOIN HierarchicalData hd ON hd.ID = ed.PARENT_ID
+)
+SELECT h.ID
+FROM HierarchicalData h
+WHERE h.HIGH = 3
+ORDER BY h.ID ASC;
