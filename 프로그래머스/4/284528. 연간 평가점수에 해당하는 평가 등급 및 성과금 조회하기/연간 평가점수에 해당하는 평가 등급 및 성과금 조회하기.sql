861d05c2963e47d4a52afd59815ac248d8ee4ccc
오찬솔 <53365713+haxr369@users.noreply.github.com>
2024-06-14 11:00:49 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,30 @@
+--  사원별 성과금 정보
+
+SELECT 
+        g.EMP_NO,
+        e.EMP_NAME,
+        CASE
+            WHEN AVG(g.SCORE) < 80
+                THEN "C"
+            WHEN AVG(g.SCORE) < 90
+                THEN "B"
+            WHEN AVG(g.SCORE) < 96
+                THEN "A"
+            ELSE "S"
+        END AS "GRADE",
+        CASE 
+            WHEN AVG(g.SCORE) < 80
+                THEN 0
+            WHEN AVG(g.SCORE) < 90
+                THEN e.SAL * 0.1
+            WHEN AVG(g.SCORE) < 96
+                THEN e.SAL * 0.15
+            ELSE e.SAL * 0.2
+        END AS "BONUS"
+FROM HR_GRADE g
+JOIN HR_EMPLOYEES e
+    ON e.EMP_NO = g.EMP_NO
+JOIN HR_DEPARTMENT d
+    ON d.DEPT_ID = e.DEPT_ID
+GROUP BY g.EMP_NO
+ORDER BY e.EMP_NO ASC;
