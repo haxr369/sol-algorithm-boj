03cd0f04533718281e12b250ee422855bccf4617
오찬솔 <53365713+haxr369@users.noreply.github.com>
2024-06-14 10:45:55 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -2,15 +2,6 @@
 -- 좌,우 테이블 둘 다 있을 경우 나오는 INNER JOIN 사용
 -- ed3는 3세대 균들
 
-SELECT ed3.ID FROM ECOLI_DATA ed3
-INNER JOIN ECOLI_DATA ed2
-    ON ed3.PARENT_ID = ed2.ID
-INNER JOIN ECOLI_DATA ed1
-    ON ed2.PARENT_ID = ed1.ID AND ed1.PARENT_ID IS NULL
-ORDER BY ed3.ID ASC;
-
---- 재귀적인 방법으로 계층구조를 만들 수 있다.
-
 WITH RECURSIVE HierarchicalData AS (
     SELECT ID, PARENT_ID, 1 AS HIGH
     FROM ECOLI_DATA
