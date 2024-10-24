2f0716aff0c2046217e93ab46941623147ffb754
오찬솔 <53365713+haxr369@users.noreply.github.com>
2024-10-24 23:58:21 +0900

    [Silver I] Title: Z, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,30 +1,29 @@
-def cutting(size,terget,now):
-  if size==1:
-    return now
-  #print(size)
-  if terget[1]<size//2:
-    if terget[0]<size//2:
-      now.append(0)
-    else:
-      terget[0]-=size//2
-      now.append(2)
-  else:
-    terget[1]-=size//2
-    if terget[0]<size//2:
-      now.append(1)
-    else:
-      terget[0]-=size//2
-      now.append(3)
-  #print("now:",now)
-  size= size//2 
-  return cutting(size,terget,now)
-n,r,c = map(int,input().split())    #배열은 2^n x 2^n
-terget_form= cutting(2**n,[r,c],[])
-terget_form.reverse()
-#print(terget_form)
 
-answer=0
-for i in range(n-1,-1,-1):
-  #print(i, terget_form[i])
-  answer+= (4**i)*terget_form[i]
-print(answer)
+N, R, C = map(int, input().split())
+#print("R : ",R," C : ",C)
+def dfs(sR, eR, sC, eC, depth, acc):
+  if(depth == N+1):
+    return acc
+  #print("depth : ",depth, " acc : ",acc)
+  stdR = (sR + eR)//2
+  stdC = (sC + eC)//2
+  #print("stdR : ",stdR, "stdC : ",stdC)
+  if(R < stdR):
+    # 0
+    if(C < stdC):
+      #print("0사분면")
+      return dfs(sR, stdR, sC, stdC, depth+1, acc)
+    else: # 1
+      #print("1사분면")
+      return dfs(sR, stdR, stdC, eC, depth+1, acc + 2**(2*N - 2*depth))
+  else:
+    # 3사분면
+    if(C < stdC):
+      #print("2사분면")
+      return dfs(stdR, eR, sC, stdC, depth+1, acc + 2*2**(2*N - 2*depth))
+    else: #4분면
+      #print("3사분면")
+      return dfs(stdR, eR, stdC, eC, depth+1, acc + 3*2**(2*N - 2*depth))
+
+ans = dfs(0, 2**N, 0, 2**N, 1, 0)
+print(ans)
